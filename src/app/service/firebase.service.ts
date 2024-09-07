0c728ddc4007b845938f4fe092e43d9901cc5691
ambadinarayan <ambadinarayan98@gmail.com>
2024-09-07 16:24:44 +0530

    firebase added with angular 18

--- src/app/service/firebase.service.ts
+++ src/app/service/firebase.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
-
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 @Injectable({
   providedIn: 'root'
 })
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAobeNDQXOnbydL5D6Uh6_EE6oNKqJKtTI",
+  authDomain: "chekonoftravancore.firebaseapp.com",
+  projectId: "chekonoftravancore",
+  storageBucket: "chekonoftravancore.appspot.com",
+  messagingSenderId: "656080923505",
+  appId: "1:656080923505:web:516325cf61e7854c0964d0",
+  measurementId: "G-BF8Q7KJ4CH"
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 export class FirebaseService {
 
   constructor() { }
+
 }
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+// module.exports = {
+  
+//   content: [
+//     "./node_modules/flowbite/**/*.js",
+//      "./node_modules/flowbite/**/*.css"
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require('flowbite/plugin')
+//   ]
+// }
+
